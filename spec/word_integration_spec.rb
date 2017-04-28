b32f7f877d1dfb6ca7665022995fbc7e1da8b8d3
sowmyadsl <sowmya.dsl@gmail.com>
2017-04-28 11:59:29 -0700

    Add integration spec for the next page and pass the next test

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -9,13 +9,19 @@ describe('/', {:type => :feature}) do
     expect(page).to have_content('Add a word ')
   end
 
-describe('/',{:type => :feature}) do
-  it('user enters the word and click on the word button') do
-    visit('/')
-    fill_in('word', :with => 'Activity')
-    click_on('Add Word')
-    expect(page).to have_content('Activity')
-  end
-
+    it('user enters the word and click on the word button') do
+      visit('/')
+      fill_in('word', :with => 'Activity')
+      click_on('Add Word')
+      expect(page).to have_content('Activity')
+    end
 end
+
+describe('/word/:id path', {:type => :feature}) do
+  it('displays textarea for new definition') do
+    visit('/word/1')
+    fill_in('define', :with => 'definition')
+    click_button('Add definition')
+    expect(page).to have_content('definition')
+  end
 end
