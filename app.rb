e58aab86542604af9dfbbb6d617c7fad98dec693
sowmyadsl <sowmya.dsl@gmail.com>
2017-04-28 11:30:05 -0700

    Add post method to clear all the words in the dictionary and add a button

--- app.rb
+++ app.rb
@@ -5,10 +5,15 @@ require('./lib/word')
 require('./lib/definition')
 require('pry')
 
-# / => / word/:id 
+# / => / word/:id
 get('/') do
   @words = Word.all()
   erb(:index)
+end
+
+post('/clear') do
+  Word.clear
+  redirect('/')
 end
 
 post('/') do
@@ -16,4 +21,17 @@ post('/') do
   word.save()
   @words = Word.all()
   erb(:index)
+end
+
+get '/word/:id' do
+  @word = Word.find(params['id'].to_i)
+  erb(:word_definition)
+end
+
+post '/word/:id' do
+  word = Word.find(params['id'].to_i)
+  new_define = Definition.new({:definition => params.fetch('define')})
+  word.add_definition(new_define)
+  @word = word
+  erb(:word_definition)
 end
