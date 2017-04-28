5a2eef422de6d7a9a8d50665c81bc4c5e3d73580
sowmyadsl <sowmya.dsl@gmail.com>
2017-04-28 09:56:54 -0700

    Add a test spec to save the inputted word to the array and test passes

--- lib/word.rb
+++ lib/word.rb
@@ -11,4 +11,8 @@ class Word
   define_singleton_method(:all) do
     @@words
   end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,12 +4,22 @@ require('definition')
 
 describe("#word") do
   it ("returns an inputted word") do
-    test_word = Word.new({:word => "activity"})
-    expect(test_word.word).to(eq("activity"))
+    test_word = Word.new({:word => "Activity"})
+    expect(test_word.word).to(eq("Activity"))
     expect(test_word.id).to(eq(1))
   end
 
-  it ("is empty at the beginning") do
-    expect(Word.all()).to(eq([]))
+  describe('.all') do
+    it ("is empty at the beginning") do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it ("adds the inputted word to the array") do
+      test_word = Word.new({:word => "Activity"})
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
   end
 end
